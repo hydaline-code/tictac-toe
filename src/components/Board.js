541ecaaa2d729f235e7851d3cc3b7616913b7c6a
hydaline-code <djougancharlen@.com>
2023-10-23 16:38:14 +0200

    Add reusable components

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,26 +2,16 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCross } from '@fortawesome/free-solid-svg-icons';
 import './styles/board.css'
+import Btn from "./Btn";
 
 const Board= () => {
+
+
   return (
 <div className="board">
-  
-<div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
+      <Btn />
+      <Btn />
+      <Btn />
 </div>
   );
 }
---
+++ src/components/Btn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles/board.css'
+
+const Btn = () =>{
+  return (
+    <div className="board-row">
+    <button className="square">1 </button>
+    <button className="square">1 </button>
+    <button className="square">1 </button>
+    </div>
+  )
+}
+
+export default Btn;
