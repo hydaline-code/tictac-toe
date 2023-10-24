1220abfba4ad65b6928369e1e104eea4f937654a
hydaline-code <djougancharlen@.com>
2023-10-24 08:29:43 +0200

    Made use of props and arrays to upddate the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,13 +5,18 @@ import './styles/board.css'
 import Btn from "./Btn";
 
 const Board= () => {
-
+// pass array of values as props
+const rowValues = [
+  [1, 2, 3],  // Values for the first row
+  [4, 5, 6],  // Values for the second row
+  [7, 8, 9]   // Values for the third row
+];
 
   return (
 <div className="board">
-      <Btn />
-      <Btn />
-      <Btn />
+      {rowValues.map((values, rowIndex) =>(
+        <Btn key={rowIndex} values={values} />
+      ))}
 </div>
   );
 }
--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './styles/board.css'
 
-const Btn = () =>{
+const Btn = ({values}) =>{
+// we map over the values array and create a button for each value.
   return (
     <div className="board-row">
-    <button className="square">1 </button>
-    <button className="square">1 </button>
-    <button className="square">1 </button>
+    {values.map((value, index) => (
+        <button key={index} className="square">{value}</button>
+      ))}
     </div>
   )
 }
