fc2a3d325a58ada2b8779cd50b22b5cc98f301ee
hydaline-code <djougancharlen@.com>
2023-10-24 16:13:40 +0200

    Add state managements to button on click

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,34 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCross } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faCircle } from '@fortawesome/free-solid-svg-icons';
 import './styles/board.css'
 import Btn from "./Btn";
 
-const Board= () => {
-// pass array of values as props
-const rowValues = [
-  ['','',''],  // Values for the first row
-  ['','',''],  // Values for the second row
-  ['','','']   // Values for the third row
-];
+const Board = () => {
+  const [buttonValues, setButtonValues] =  useState(Array(9).fill(null))   //or useState([null, null, null, null, null, null, null, null, null]);
+  const [btnIsNext, setbtnIsNext] = useState(false); //is a boolean variable that keeps track of whose turn it is. 
 
+  const handleClick = (index) => {
+    const newButtonValues = [...buttonValues]; // makes a copy of the array you can use  buttonValues.slice();
+    if (newButtonValues[index] === null) {
+    newButtonValues[index] =btnIsNext ? <FontAwesomeIcon icon={faTimes}  className="icon"/> :
+    <FontAwesomeIcon icon={faCircle} className="icon" />; //When xIsNext is true, it's "X's" turn, and when it's false, it's "O's" turn.
+    setButtonValues(newButtonValues);
+    setbtnIsNext(!btnIsNext);
+    
+    }
+  };
+  
   return (
-<div className="board">
-      {rowValues.map((values, rowIndex) =>(
-        <Btn key={rowIndex} values={values} />
+    <div className="board">
+      <div className="grid-container">
+      {buttonValues.map((value, index) => (
+        <Btn key={index} value={value} onClick={() => handleClick(index)} />
       ))}
-</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Board;
--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import './styles/board.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
 
-const Btn = () =>{
-const [buttonValues, setButtonValues] = useState(["", "", ""]);
 
-const handleClick=(index) =>{
-  const newbtnValues= [...buttonValues];
-  newbtnValues[index] = <FontAwesomeIcon icon={faClose} />;
-  setButtonValues(newbtnValues);
-}
+const Btn = ({ value, onClick }) =>{
+
   return (
     <div className="board-row">
-    {buttonValues.map((value, index) => (
-        <button key={index} className="square" onClick={() => handleClick(index)}>{value}</button>
-      ))}
+   <button
+        className="square"
+        onClick={onClick}
+      >
+        {value}
+      </button>
     </div>
   )
 }
